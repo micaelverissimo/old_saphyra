645888400b4bf2efb27d73ffaf36a64d44593d35
jodafons <jodafons@lps.ufrj.br>
2019-06-19 00:03:55 -0300

    remove old scripts

--- setup.sh
+++ setup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+export Athena_SETUP=off
+export CPLUS_INCLUDE_PATH="$CPLUS_INCLUDE_PATH:/usr/include/python2.7/"
+export LC_ALL=''
+export RCM_NO_COLOR=0
+export RCM_GRID_ENV=0
+
+ln -sf standalone/CMakeLists.txt CMakeLists.txt
+
+
 cd build
 rm -rf lib
 mkdir lib
--- setup_standalone.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export Athena_SETUP=off
-export CPLUS_INCLUDE_PATH="$CPLUS_INCLUDE_PATH:/usr/include/python2.7/"
-export LC_ALL=''
-export RCM_NO_COLOR=0
-export RCM_GRID_ENV=0
-
-
-ln -sf standalone/CMakeLists.txt CMakeLists.txt
