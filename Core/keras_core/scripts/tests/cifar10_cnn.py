1c0de265ac649dac24c4a46064c98373a74a9bcd
jodafons <jodafons@lps.ufrj.br>
2019-06-22 14:08:46 +0200

    update the ml base docker constructor

--- Core/keras_core/scripts/tests/cifar10_cnn.py
+++ Core/keras_core/scripts/tests/cifar10_cnn.py
@@ -19,7 +19,7 @@ epochs = 1
 data_augmentation = False
 num_predictions = 20
 save_dir = os.path.join(os.getcwd(), 'saved_models')
-model_name = 'keras_cifar10_trained_model.h5'
+model_name = 'keras_cifar10_trained_model.h5.tgz'
 
 # The data, split between train and test sets:
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
