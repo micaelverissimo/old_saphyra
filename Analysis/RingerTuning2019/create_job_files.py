83aa4aa68598aa0b842d6c92eeba3ab7e9a7355d
jodafons <jodafons@lps.ufrj.br>
2019-07-06 23:42:26 +0200

    add 2019 tuning scripts

--- Analysis/RingerTuning2019/create_job_files.py
+++ Analysis/RingerTuning2019/create_job_files.py
@@ -11,26 +11,28 @@ from saphyra import PreProcChain_v1, Norm1
 pp = PreProcChain_v1( [Norm1()] )
 
 
-
+modelCol = []
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation
+
+
 # creating the keras model first
-model = Sequential()
-model.add(Dense(100, input_shape=(100,)))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(5))
-model.add(Activation('relu'))
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
-modelCol = [model]
-
-
+for n in range(5,10+1):
+  
+  model = Sequential()
+  model.add(Dense(100, input_shape=(100,)))
+  model.add(Activation('tanh'))
+  model.add(Dropout(0.5))
+  model.add(Dense(n))
+  model.add(Activation('tanh'))
+  model.add(Dense(1))
+  model.add(Activation('sigmoid'))
+  modelCol.append(model)
 
 
 from sklearn.model_selection import StratifiedKFold, KFold
-#kf = StratifiedKFold(n_splits=10, random_state=1234, shuffle=True)
-kf = KFold(n_splits=10, random_state=1234, shuffle=True)
+kf = StratifiedKFold(n_splits=10, random_state=1234, shuffle=True)
+#kf = KFold(n_splits=10, random_state=1234, shuffle=True)
 
 
 
@@ -39,8 +41,9 @@ pandas( models = modelCol,
         ppChain = pp,
         crossVal = kf,
         nInits = 10,
-        nIninsPerJob = 10,
+        nInitsPerJob = 1,
         sortBounds =  PythonLoopingBounds(10),
-        nSortsPerJob = 5,
+        nSortsPerJob = 1,
         outputFolder = 'job_test')
+
 
--- Analysis/RingerTuning2019/run_grid.sh
+++ Analysis/RingerTuning2019/run_grid.sh
@@ -6,8 +6,23 @@ run_grid_tuning.py \
   -d user.jodafons.grid_test_dataFile \
   -pp user.jodafons.grid_test_ppFile \
   --containerImage docker://jodafons/ml-base:latest  \
-  -o user.jodafons.grid_test.t0001 \
-  -j job_tuning.py
+  -o user.jodafons.grid_test.t002o \
+  -j /home/atlas/saphyra/Core/saphyra/scripts/standalone/job_tuning.py \
+  --site ANALY_BNL_SHORT \
+  #--site AUTO \
+
+
+#run_grid_tuning.py \
+#  -c user.jodafons.job_container_10sorts_10inits \
+#  -m user.jodafons.model_MLP_hidden_layer_n5to10 \
+#  -x user.jodafons.crossval.skf_10sorts \
+#  -d user.jodafons.grid_test_dataFile \
+#  -pp user.jodafons.pp.norm1 \
+#  --containerImage docker://jodafons/ml-base:latest  \
+#  -o user.jodafons.grid_test_et4_eta4.t0001 \
+#  -j /home/atlas/saphyra/Core/saphyra/scripts/standalone/job_tuning.py \
+#  --site ANALY_BNL_SHORT \
+#  #--site AUTO \
 
 
 
