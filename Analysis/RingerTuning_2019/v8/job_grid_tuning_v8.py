d83e2caec5c6a591352e90ee94ab7876f45ee645
jodafons <jodafons@lps.ufrj.br>
2019-08-08 04:55:00 +0200

    remove junk files

--- Analysis/RingerTuning_2019/v8/job_grid_tuning_v8.py
+++ Analysis/RingerTuning_2019/v8/job_grid_tuning_v8.py
@@ -16,30 +16,23 @@ parser = argparse.ArgumentParser()
 
 
 
-parser.add_argument('-c','--configFile', action='store',
+parser.add_argument('-c','--configFile', action='store', 
         dest='configFile', required = True,
             help = "The job config file that will be used to configure the job (sort and init).")
 
-parser.add_argument('-o','--outputFile', action='store',
+parser.add_argument('-o','--outputFile', action='store', 
         dest='outputFile', required = False, default = None,
             help = "The output tuning name.")
 
-parser.add_argument('-x','--crossValFile', action='store',
-        dest='crossValFile', required = False, default = None,
-            help = "The crossval file.")
 
-parser.add_argument('-d','--dataFile', action='store',
+parser.add_argument('-d','--dataFile', action='store', 
         dest='dataFile', required = False, default = None,
             help = "The data/target file used to train the model.")
 
 
-parser.add_argument('-p','--ppFile', action='store',
-        dest='ppFile', required = False, default = None,
-            help = "The preproc chain file.")
-
-parser.add_argument('-m','--modelFile', action='store',
-        dest='modelFile', required = False, default = None,
-            help = "The Keras model collection file.")
+parser.add_argument('-r','--refFile', action='store', 
+        dest='refFile', required = False, default = None,
+            help = "The reference file.")
 
 
 
@@ -50,58 +43,68 @@ if len(sys.argv)==1:
 args = parser.parse_args()
 
 
-
 # Reading data and get all information needed
 # by the tuning proceding
 raw = load(args.dataFile)
 data = raw['data'][:,1:101]
 target = raw['target']
 pileup = raw['data'][:,0]
+del raw
 
 
 ref_target = [
-              #('tight_v8'       , 'T0HLTElectronRingerTight_v8'     ),
-              #('medium_v8'      , 'T0HLTElectronRingerMedium_v8'    ),
-              #('loose_v8'       , 'T0HLTElectronRingerLoose_v8'     ),
-              #('vloose_v8'      , 'T0HLTElectronRingerVeryLoose_v8' ),
-              #('tight_v6'       , 'T0HLTElectronRingerTight_v6'     ),
-              #('medium_v6'      , 'T0HLTElectronRingerMedium_v6'    ),
-              #('loose_v6'       , 'T0HLTElectronRingerLoose_v6'     ),
-              #('vloose_v6'      , 'T0HLTElectronRingerVeryLoose_v6' ),
               ('tight_cutbased' , 'T0HLTElectronT2CaloTight'        ),
               ('medium_cutbased', 'T0HLTElectronT2CaloMedium'       ),
               ('loose_cutbased' , 'T0HLTElectronT2CaloLoose'        ),
               ('vloose_cutbased', 'T0HLTElectronT2CaloVLoose'       ),
               ]
 
+
+
+from saphyra import ReferenceReader
+ref_obj = ReferenceReader().load(args.refFile)
+
 posproc = [Summary()]
-
 obj = PileupFit( "PileupFit", pileup )
-
 # Calculate the reference for each operation point
 # using the ringer v6 tuning as reference
 for ref in ref_target:
-  d = raw['data'][:,np.where(raw['features'] == ref[1])[0][0]-1]
-  d_s = d[target==1]
-  d_b = d[target!=1]
-  pd = sum(d_s)/float(len(d_s))
-  fa = sum(d_b)/float(len(d_b))
+  pd = ref_obj.getSgnPassed(ref[0]) / float(ref_obj.getSgnTotal(ref[0]))
+  fa = ref_obj.getBkgPassed(ref[0]) / float(ref_obj.getBkgTotal(ref[0]))
   obj.add( ref[0], ref[1], pd, fa )
-
 posproc.append( obj )
 
-# remove raw data
-del raw
+
+
+
+from sklearn.model_selection import StratifiedKFold, KFold
+kf = StratifiedKFold(n_splits=10, random_state=512, shuffle=True)
+
+from saphyra import PreProcChain_v1, Norm1
+pp = PreProcChain_v1( [Norm1()] )
+
+
+
+def get_model( neurons ):
+  modelCol = []
+  for n in neurons:
+    model = Sequential()
+    model.add(Dense(n, input_shape=(100,), activation='tanh', kernel_initializer='random_uniform', bias_initializer='random_uniform'))
+    model.add(Dense(1, activation='linear', kernel_initializer='random_uniform', bias_initializer='random_uniform'))
+    model.add(Activation('tanh'))
+    modelCol.append(model)
+  return modelCol
 
 
 # Create the job
-job = PandasJob(  job       = args.configFile,
-                  models    = args.modelFile,
+job = PandasJob(  job       = args.configFile, 
+                  models    = get_model( [2,3,4,5,6,7,8,9,10] ),
+                  #loss      = 'binary_crossentropy',
                   loss      = 'mse',
                   metrics   = ['accuracy'],
                   epochs    = 5000,
-                  ppChain   = args.ppFile,
-                  crossval  = args.crossValFile,
+                  ppChain   = pp,
+                  crossval  = kf,
                   outputfile= args.outputFile,
                   data      = data,
                   target    = target,
@@ -114,7 +117,6 @@ job.callbacks += [sp(patience=25, verbose=True, save_the_best=True)]
 job.initialize()
 job.execute()
 job.finalize()
-
 
 
 
--- Analysis/RingerTuning_2019/v8/run_grid_tuning_v8.py
+++ Analysis/RingerTuning_2019/v8/run_grid_tuning_v8.py
@@ -1,18 +1,14 @@
 
 command = """run_grid_tuning.py \
       -c user.jodafons.job_config_10sorts_10inits \
-      -m user.jodafons.models_mlp_n5to20 \
-      -x user.jodafons.StratifiedKFold.10_sorts \
       -d user.jodafons.data17_13TeV.AllPeriods.sgn.probes_lhmedium_EGAM1.bkg.VProbes_EGAM7.GRL_v97_et{et}_eta{eta}.npz \
-      -pp user.jodafons.preproc.norm1 \
       --containerImage docker://jodafons/ml-base:latest  \
       -o user.jodafons.data17_13TeV.Allperiods.sgn.probes_lhmedium_EG1.bkg.VProbes_EG7.mlp.ringer_v8_et{et}_eta{eta}.r8 \
       -j /home/atlas/saphyra/Analysis/RingerTuning_2019/v8/job_tuning_v8.py \
-      --site AUTO \
-      --memory 10000 \
-      --nCore 8 """
+      --site ANALY_BNL_LONG \
+      """
 
-#--site ANALY_BNL_LONG \
+
 import os
 
 bins = [
