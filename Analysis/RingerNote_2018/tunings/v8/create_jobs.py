34a6276a32859c9cd7edf58e6b13739ca131edfc
jodafons <jodafons@lps.ufrj.br>
2019-10-17 16:48:37 +0200

    update jobs from v8

--- Analysis/RingerNote_2018/tunings/v8/create_jobs.py
+++ Analysis/RingerNote_2018/tunings/v8/create_jobs.py
@@ -8,7 +8,7 @@ from saphyra import *
 # ppChain
 from saphyra import PreProcChain_v1, Norm1, ReshapeToConv1D
 
-pp = PreProcChain_v1( [Norm1(), ReshapeToConv1D()] )
+pp = PreProcChain_v1( [Norm1()] )
 
 
 from tensorflow.keras.models import Sequential
@@ -18,7 +18,7 @@ from tensorflow.keras.layers import Dense, Dropout, Activation, Conv1D, Flatten
 
 def get_model( ):
   modelCol = []
-  for n in range(1,10+1):
+  for n in range(2,15+1):
     model = Sequential()
     model.add(Dense(n, input_shape=(100,), activation='tanh', kernel_initializer='random_uniform', bias_initializer='random_uniform'))
     model.add(Dense(1, activation='linear', kernel_initializer='random_uniform', bias_initializer='random_uniform'))
@@ -39,11 +39,11 @@ createPandaJobs( models        = get_model(),
         ppChain       = pp,
         crossVal      = kf,
         nInits        = 10,
-        nInitsPerJob  = 1,
+        nInitsPerJob  = 2,
         sortBounds    = PythonLoopingBounds(10),
         nSortsPerJob  = 1,
         nModelsPerJob = 5,
-        outputFolder  = 'job_config.ringer.v8.mlp1to10.10sorts.10inits'
+        outputFolder  = 'job_config.ringer.v8.mlp2to15.10sorts.10inits'
         )
 
 
