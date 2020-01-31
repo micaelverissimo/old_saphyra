fcc08373be38cce7390d2745001d32832dd2f985
jodafons <jodafons@lps.ufrj.br>
2020-01-31 15:53:44 -0300

    fix cmake

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 
 
-export Athena_SETUP=off
 export LC_ALL=''
 export RCM_NO_COLOR=0
 export RCM_GRID_ENV=0
@@ -8,20 +7,18 @@ export SAPHYRA_PATH=`pwd`
 
 
 
-rm -rf .python_dir
-mkdir .python_dir
-cd .python_dir
-mkdir python
-cd python
-ln -s ../../Gaugi/python Gaugi
-ln -s ../../saphyra/python saphyra
-ln -s ../../External/orchestra/python orchestra
-ln -s ../../External/ringerdb/python ringerdb
-ln -s ../../External/monet/python monet
-ln -s ../../External/pytex/python pytex
-ln -s ../../Tools/panda/python panda
+rm -rf .pythonpaths
+mkdir .pythonpaths
+cd .pythonpaths
+
+
+ln -s ../Gaugi/python Gaugi
+ln -s ../saphyra/python saphyra
+ln -s ../External/monet/python monet
+ln -s ../External/pytex/python pytex
+ln -s ../Tools/panda/python panda
 
 export PYTHONPATH=`pwd`:$PYTHONPATH
-cd ../../
+cd ../
 
 
