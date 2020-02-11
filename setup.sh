dc9bba6f2a1c42182e6d1861c58aa3efd289b98b
jodafons <jodafons@lps.ufrj.br>
2020-02-10 22:55:24 -0300

    reshape

--- setup.sh
+++ setup.sh
@@ -5,19 +5,12 @@ export RCM_NO_COLOR=0
 export RCM_GRID_ENV=0
 export SAPHYRA_PATH=`pwd`
 
-
-
-rm -rf .pythonpaths
-mkdir .pythonpaths
-cd .pythonpaths
-
+rm -rf .__python__
+mkdir .__python__
+cd .__python__
 
 ln -s ../Gaugi/python Gaugi
-ln -s ../External/ringerdb/python ringerdb
 ln -s ../saphyra/python saphyra
-# ln -s ../External/monet/python monet
-# ln -s ../External/pytex/python pytex
-#ln -s ../Tools/panda/python panda
 
 export PYTHONPATH=`pwd`:$PYTHONPATH
 cd ../
